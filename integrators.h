26bdba9ea9f35c1f268b686c219565153b69ca3b
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2020-10-15 15:56:16 +0300

    Add integrator class interface

---
+++ integrators.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <functional>
+
+class integrator
+{
+	typedef std::function<double(double, double)> ddd_function;
+
+private:
+	struct Point_info
+	{
+		double curr_x;
+		double curr_v;
+		double curr_v_2;
+		double curr_step;
+		double error;
+	};
+
+	Point_info point_info;
+	double mul_count;
+	double div_count;
+	double eps;
+
+public:
+	integrator(ddd_function func, double x, double v, double step, double eps_);
+	double runge_kutta_4();
+	Point_info next_point();
+	Point_info next_point_with_step_control();
+};
+
